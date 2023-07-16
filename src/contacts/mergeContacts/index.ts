1800d439b1720762ffaca13a8af9ef3742af632f
varshneyabhushan <me@bhushan.fun>
2023-07-17 03:00:36 +0530

    merge contacts

--- src/contacts/mergeContacts/index.ts
+++ src/contacts/mergeContacts/index.ts
@@ -1,7 +1,11 @@
+import { ContactsTable, LinkPrecedence } from "..";
 import DatabaseConnection from "../../database";
 
-export default function makeMergeContacts(databaseConnection : DatabaseConnection) {
-    return async function(contact1 : number, contact2 : number) : Promise<number> {
-        return Promise.reject('not yet implemented')
+export default function makeMergeContacts(databaseConnection: DatabaseConnection) {
+    return async function (contact1: number, contact2: number): Promise<number> {
+        return databaseConnection.query(
+            `UPDATE ${ContactsTable} SET linkedId=${contact1}, linkPrecedence='${LinkPrecedence.secondary}' WHERE id=${contact2} OR linkedId=${contact2}`,
+        )
+        .then(() => contact1)
     }
 }
