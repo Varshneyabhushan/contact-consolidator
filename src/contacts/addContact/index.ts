99fa095ee870fcf4de875f6dffb8db5f4fef933b
varshneyabhushan <me@bhushan.fun>
2023-07-17 01:48:45 +0530

    addContact with linkPrecendence

--- src/contacts/addContact/index.ts
+++ src/contacts/addContact/index.ts
@@ -1,9 +1,11 @@
 import { Contact, ContactsTable, LinkPrecedence } from "..";
 import DatabaseConnection from "../../database";
 
-interface NewContact {
-    phoneNumber?: string;
+export interface NewContact {
     email?: string;
+    phoneNumber?: string;
+    linkedId?: number;
+    linkPrecedence?: LinkPrecedence;
 }
 
 export default function makeContactAdder(connection: DatabaseConnection) {
@@ -13,7 +15,8 @@ export default function makeContactAdder(connection: DatabaseConnection) {
         return connection.insert(ContactsTable, {
             email: newContact.email,
             phoneNumber: newContact.phoneNumber,
-            linkPrecedence : LinkPrecedence.primary,
+            linkedId: newContact.linkedId,
+            linkPrecedence: newContact.linkPrecedence ?? LinkPrecedence.primary,
             createdAt: timeNow,
             updatedAt: timeNow,
         } as Contact)
