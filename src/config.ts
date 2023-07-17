72ced9fdea7727eaf5fbdc7457ec04ad6d6f8b68
varshneyabhushan <me@bhushan.fun>
2023-07-18 03:13:29 +0530

    added rateLimiter

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,12 @@ import { toNumber } from './utils/conversions';
 config();
 
 const port = toNumber(process.env.PORT ?? '80') ?? 80;
+const requestsPerMinute = toNumber(process.env.REQUESTS_PER_MINUTE ?? '100') ?? 100; 
 
 const configJSON = {
     app: {
         port,
+        requestsPerMinute,
     },
     database : {
         host : process.env.HOST ?? "",
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import makeConsolidateRoute from "./consolidate";
 import DatabaseConnection from "../database";
+import rateLimit from "express-rate-limit"
 
 export interface AppConfig {
     port: number;
+    requestsPerMinute : number;
 }
 
 export interface RouterError {
-    message : string;
-    payload : any;
-    type : string;
+    message: string;
+    payload: any;
+    type: string;
 }
 
 const startRouter = (
     appConfig: AppConfig,
-    databaseConnection : DatabaseConnection,
+    databaseConnection: DatabaseConnection,
 ): Promise<void> => {
     const app = express();
 
@@ -22,8 +24,15 @@ const startRouter = (
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
+    // Rate limiter configuration: requests per minute
+    const minute = 60* 1000
+    const limiter = rateLimit({
+        windowMs: minute,
+        max: appConfig.requestsPerMinute, // Limit each IP to 100 requests per minute
+    });
+
     //TODO implement authentication
-    app.post("/identify", makeConsolidateRoute(databaseConnection))
+    app.post("/identify", limiter, makeConsolidateRoute(databaseConnection))
 
     app.use('*', (_, res) => res.status(404).json({ message: 'invalid route' }));
 
